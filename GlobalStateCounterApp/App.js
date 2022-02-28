b1cd16f6ea98e865280c171beecd6099a287e322
Hareesh S <hareesh.s.teja@gmail.com>
2022-02-28 17:23:17 +0530

    Init New Assignment

---
+++ GlobalStateCounterApp/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import CounterScreen from './src/screens/CounterScreen';
+
+export default function App() {
+  return (
+    <>
+      <CounterScreen />
+    </>
+  )
+}
---
+++ GlobalStateCounterApp/src/components/CustomButton/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, TouchableOpacity, Text } from 'react-native';
+
+export default function CustomButton({ PropStylesButton, PropStylesText, PropText, PropFunction }) {
+    return (
+        <>
+            <TouchableOpacity style={[styles.container, PropStylesButton]} activeOpacity={0.9} onPress={PropFunction}>
+                <Text style={[styles.text, PropStylesText]}>{PropText}</Text>
+            </TouchableOpacity>
+        </>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        elevation: 10,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },
+
+    text: {
+        fontSize: 70,
+    }
+
+});
