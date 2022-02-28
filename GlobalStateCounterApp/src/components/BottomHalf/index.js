57d6ec993c3ce639c4f04e96f46b386791f4b0e7
Hareesh S <hareesh.s.teja@gmail.com>
2022-02-28 20:29:52 +0530

    Switched to Vector Icons. Needs Global Context Still

--- GlobalStateCounterApp/src/components/BottomHalf/index.js
+++ GlobalStateCounterApp/src/components/BottomHalf/index.js
@@ -15,11 +15,11 @@ export default function BottomHalf() {
 
     return (
         <>
-                <View style={[styles.buttonRow, { width: windowWidth - 20, height: (windowHeight * 0.5) - 15 }]}>
-                    <CustomButton PropText="-" PropStylesButton={[styles.subButton, { width: (windowWidth / 2) - 40, height: (windowHeight * 0.5) - 55 }]} PropStylesText={styles.subButtonText} PropFunction={() => setCount(count - 1)} />
+            <View style={[styles.buttonRow, { width: windowWidth - 20, height: (windowHeight * 0.5) - 15 }]}>
+                <CustomButton PropIconName={'minus'} PropStylesButton={[styles.button, { width: (windowWidth / 2) - 40, height: (windowHeight * 0.5) - 55 }]} PropStylesText={styles.buttonIcon} PropFunction={() => setCount(count - 1)} />
 
-                    <CustomButton PropText="+" PropStylesButton={[styles.addButton, { width: (windowWidth / 2) - 40, height: (windowHeight * 0.5) - 55 }]} PropStylesText={styles.addButtonText} PropFunction={() => setCount(count + 1)} />
-                </View>
+                <CustomButton PropIconName={'plus'} PropStylesButton={[styles.button, { width: (windowWidth / 2) - 40, height: (windowHeight * 0.5) - 55 }]} PropStylesText={styles.buttonIcon} PropFunction={() => setCount(count + 1)} />
+            </View>
             {/* </View> */}
         </>
     )
@@ -48,29 +48,17 @@ const styles = StyleSheet.create({
 
     // ((windowWidth - 10) - 2*((windowWidth/2)-40))/3
 
-    addButton: {
+    button: {
         backgroundColor: buttonColor,
         // width: (windowWidth/2)-40,
         borderRadius: cornerRadius,
 
     },
 
-    subButton: {
-        backgroundColor: buttonColor,
-        // width: (windowWidth/2)-40,
-        borderRadius: cornerRadius,
-
-    },
-
-    addButtonText: {
+    buttonIcon: {
         fontFamily: font,
         color: fontColor,
-
-    },
-
-    subButtonText: {
-        fontFamily: font,
-        color: fontColor,
+        fontSize: 50,
 
     },
 
--- GlobalStateCounterApp/src/components/CustomButton/index.js
+++ GlobalStateCounterApp/src/components/CustomButton/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { StyleSheet, TouchableOpacity, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
 
-export default function CustomButton({ PropStylesButton, PropStylesText, PropText, PropFunction }) {
+
+export default function CustomButton({ PropStylesButton, PropStylesText, PropIconName, PropFunction }) {
     return (
         <>
             <TouchableOpacity style={[styles.container, PropStylesButton]} activeOpacity={0.9} onPress={PropFunction}>
-                <Text style={[styles.text, PropStylesText]}>{PropText}</Text>
+                {/* <Text style={[styles.text, PropStylesText]}>{PropText}</Text> */}
+                <Icon name={PropIconName} style={[styles.text, PropStylesText]}/>
             </TouchableOpacity>
         </>
     )
@@ -20,8 +23,8 @@ const styles = StyleSheet.create({
 
     },
 
-    text: {
-        fontSize: 70,
-    }
+    // text: {
+    //     fontSize: 70,
+    // }
 
 });
