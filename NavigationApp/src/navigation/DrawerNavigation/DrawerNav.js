dd8c6ee28894a484c5eb02f278f19780e15ad896
Hareesh S <hareesh.s.teja@gmail.com>
2022-03-08 17:44:47 +0530

    Init NavigationApp

---
+++ NavigationApp/src/navigation/DrawerNavigation/DrawerNav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+const Drawer = createDrawerNavigator();
+
+export default function DrawerNav() {
+    return(
+    <Drawer.Navigator>
+        
+    </Drawer.Navigator>
+    )
+}
