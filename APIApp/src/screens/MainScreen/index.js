1e0ac050fcc487cc30347b5ae3ebddcf0d19e573
Hareesh S <hareesh.s.teja@gmail.com>
2022-04-17 20:09:57 +0530

    Working Update

--- APIApp/src/screens/MainScreen/index.js
+++ APIApp/src/screens/MainScreen/index.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
-import { View, Image, Alert, Text, ActivityIndicator, FlatList } from 'react-native';
+import { View, Image, Alert, Text, ActivityIndicator, FlatList, Linking } from 'react-native';
 import { styles } from './styles';
 import axios from 'axios';
 import Icon from 'react-native-vector-icons/AntDesign';
 import CustomInput from '../../components/CustomInput';
-import NYTLogo from '../../assets/images/NYTLogo.svg';
 
 export default function MainScreen() {
     const [search, setSearch] = useState('');
@@ -12,7 +11,6 @@ export default function MainScreen() {
     const [isLoading, setIsLoading] = useState(false);
 
     const apiKey = 'lhDgjGGhYkQwh4sFYzvgRsoV98uQh8Ib';
-    const LogoDim = 220;
 
     const nullSearch = () => {
         Alert.alert(
@@ -28,8 +26,6 @@ export default function MainScreen() {
             // const api=`https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=%22The%20New%20York%20Times%22&q=${search}&sort=newest&api-key=${apiKey}`;
             // console.log(api);
             const response = await axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=%22The%20New%20York%20Times%22&q=${search}&sort=newest&api-key=${apiKey}`);
-            console.log(response.status);
-            // console.log(response.data);
             setData(response.data);
             setIsLoading(false);
         } else {
@@ -47,7 +43,9 @@ export default function MainScreen() {
                         <Text style={styles.articleByline}>{data["response"]["docs"][0]["lead_paragraph"]}</Text>
                     </View>
                 </View>
-                <Text style={styles.articleLink}>{data["response"]["docs"][0]["web_url"]}</Text>
+                <Text style={styles.articleLink} onPress={() => {
+                    Linking.openURL(`${data["response"]["docs"][0]["web_url"]}`);
+                }} >{data["response"]["docs"][0]["web_url"]}</Text>
 
             </View>
         } else {
@@ -60,7 +58,6 @@ export default function MainScreen() {
         <>
             <Image source={require('../../assets/images/Gradient.png')} style={styles.bg} />
             <View style={styles.container}>
-                {/* <NYTLogo width={LogoDim} height={LogoDim} style={styles.NYTLogo} /> */}
                 <View style={styles.searchBox}>
                     <CustomInput propValue={search} propOnChangeText={setSearch} propPlaceholder={'Search'} propContainerStyle={styles.searchBar} propPlaceholderTextColour={'white'} propTextInputStyle={styles.searchText} />
                     <Icon name='search1' onPress={onSearch} style={styles.searchButton} />
